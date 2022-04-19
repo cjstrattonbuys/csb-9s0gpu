96a1bf9b27d8cf28682c276b6ac4bfe444b18419
Code Sandbox <hello@codesandbox.io>
2022-04-19 18:33:15 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.0fbbb1968055bdb2d0f92dfdc6566747.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "84041d9d239846bcc011",
+    "url": "https://cjstrattonbuys.github.io/csb-9s0gpu/static/css/main.e36b6129.chunk.css"
+  },
+  {
+    "revision": "84041d9d239846bcc011",
+    "url": "https://cjstrattonbuys.github.io/csb-9s0gpu/static/js/main.d7079dbe.chunk.js"
+  },
+  {
+    "revision": "a48924826f4e8f4e19bb",
+    "url": "https://cjstrattonbuys.github.io/csb-9s0gpu/static/js/runtime~main.9b0f9dc8.js"
+  },
+  {
+    "revision": "09fc09cca39aeb28cc7e",
+    "url": "https://cjstrattonbuys.github.io/csb-9s0gpu/static/js/2.28d8c36f.chunk.js"
+  },
+  {
+    "revision": "1f7e6bdded2fbec0c5985db5d8264380",
+    "url": "https://cjstrattonbuys.github.io/csb-9s0gpu/index.html"
+  }
+];
